63e32b960ca82d8c1f7c12d931b6ef0a3d1aef6c
Ganneesh <151457983+Ganneesh@users.noreply.github.com>
2025-06-27 23:14:25 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,12 +25,7 @@ globalTeardown: require.resolve('./globalTeardown'),
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['list'],
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    ['allure-playwright']
-  ]
-  ,
+  reporter: [['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
