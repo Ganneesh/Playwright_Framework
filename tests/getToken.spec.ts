c84b05b2a53d55e443fdc0f1df86cb055d67ace7
Ganesh <gsmadane7@gmail.com>
2025-07-13 23:00:03 +0530

    chnges

--- tests/getToken.spec.ts
+++ tests/getToken.spec.ts
@@ -1,24 +1,24 @@
-import { test, expect, request } from '@playwright/test';
-import { getSavedToken } from '../support/utils/authHelper.ts'
+// import { test, expect, request } from '@playwright/test';
+// import { getSavedToken } from '../support/utils/authHelper.ts'
 
 
 
-test.describe('Demo Test cases',()=>{
+// test.describe('Demo Test cases',()=>{
 
-    test('Get users with saved token', async () => {
-  const token = getSavedToken();
+//     test('Get users with saved token', async () => {
+//   const token = getSavedToken();
 
-  const context = await request.newContext({
-    baseURL: process.env.BASE_URL,
-    extraHTTPHeaders: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+//   const context = await request.newContext({
+//     baseURL: process.env.BASE_URL,
+//     extraHTTPHeaders: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//   });
 
-  const response = await context.get('/api/users');
-  expect(response.ok()).toBeTruthy();
-});
+//   const response = await context.get('/api/users');
+//   expect(response.ok()).toBeTruthy();
+// });
 
-})
+// })
 
 
